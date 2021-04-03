191c70659c5a271113389cc9421db42419718ab0
Byeraon <nikitka.crasnov2013@mail.ru>
2021-04-03 12:23:55 +0300

    First Upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Nav from './nav';
+import Profile from './Profile/profile';
+import Dialogs from './Dialogs/dialogs';
+import News from './News/news';
+import Settings from './Settings/settings';
+import Music from './Music/music';
+import s from './content.module.css';
+import f from './App.module.css';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
+const App = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className={f.page}>
+        <Nav></Nav>
+        <div className={s.blkpage}>
+          <Route path="/dialogs" render={() => <Dialogs state={props.state.messagesDataPage} />}></Route>
+          <Route path="/profile" render={ () => <Profile state={props.state.profileDataPage} addPost={props.addPost}/>}></Route>
+          <Route path="/news" render={ () => <News />}></Route>
+          <Route path="/settings" render={ () => <Settings />}></Route>
+          <Route path="/music" render={ () => <Music />}></Route>
+        </div>
+      </div>
+    </BrowserRouter>
   );
-}
+};
+
 
 export default App;
